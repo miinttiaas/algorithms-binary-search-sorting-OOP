af3109b0d21cc5d4b347238c9a8be7b5dea27bc2
Anastasia <synyshyna.anastasiia@chnu.edu.ua>
2024-05-14 20:34:38 +0300

    sorting_by_choice

--- sorting№1.py
+++ sorting№1.py
@@ -1,1 +1,16 @@
+# list_of_groups = [5,2,21,1,3,4,5,6,7,8,9,10]
+# for n in range(0,len(list_of_groups)):
+#     for j in range(n+1,len(list_of_groups)):
+#         if list_of_groups[n]>=list_of_groups[j]:
+#             list_of_groups[n],list_of_groups[j] = list_of_groups[j],list_of_groups[n]
+# print(list_of_groups)
+# print(list_of_groups == sorted(list_of_groups))
 
+def sorting_by_length(array):
+    for n in range(len(array)):
+        for i in range(n+1, len(array)):
+            if array[n] <= array[i]:
+                array[i], array[n] = array[n], array[i]
+        print(array)
+    return array == sorted(array,reverse=True)
+print(sorting_by_length([5,2,2,21,1,3,4,5,6,7,8,9,10]))
