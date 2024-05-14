99ce812bc46eb4d4a5d3d309c7fc35e532f80a26
Anastasia <synyshyna.anastasiia@chnu.edu.ua>
2024-05-14 19:10:28 +0300

    binary search

--- binary_search.py
+++ binary_search.py
@@ -1,1 +1,11 @@
-
+def binary_search(goal, low, high,counter=0):
+    half = (high+low)//2
+    print(f"№{counter} {half}")
+    if half > goal:
+        counter += 1
+        return  1 + binary_search(goal, low, half,)
+    elif half < goal:
+        counter += 1
+        return 1 + binary_search(goal, half, high,counter)
+    return counter
+print(binary_search(10, 1, 100))
